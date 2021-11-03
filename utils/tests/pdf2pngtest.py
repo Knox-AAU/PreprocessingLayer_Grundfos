53932472fc6f01a66c57eb9807f6c69b0425855a
Nick / AFK <ndja19@student.aau.dk>
2021-11-03 14:11:15 +0100

    Added unittest for writing to text file

--- utils/tests/pdf2pngtest.py
+++ utils/tests/pdf2pngtest.py
@@ -4,6 +4,7 @@ import unittest
 
 import config_data
 import utils.pdf2png as p2p
+from config_data import config
 
 
 class UtilsTest(unittest.TestCase):
@@ -27,6 +28,22 @@ class UtilsTest(unittest.TestCase):
 
         with self.assertWarns(RuntimeWarning):
             p2p.multi_convert_dir_to_files(file, out_dir)
+
+
+    def test_write_corrupt_to_file(self):
+        os.environ["GRUNDFOS_INVALID_INPUT_FOLDER"] = str(os.path.join(os.path.abspath(os.curdir), "test_invalid"))
+        config_data.set_config_data_from_envs()
+        input = "CORRUPT_PDF_NAME_EXAMPLE.pdf"
+        files = []
+        files.append(input)
+
+        p2p.write_invalid_pdf_list(files)
+        file = open(os.path.join(config["INVALID_INPUT_FOLDER"], "invalids.txt"),"r")
+        output = file.read()
+        file.close()
+        self.assertEqual(input + " \n", output)
+
+
 
 if __name__ == '__main__':
     unittest.main()
