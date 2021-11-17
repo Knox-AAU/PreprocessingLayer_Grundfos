658932a3f43821766ca7fa68f5c5a8ba313366b6
itDaveDK <git@david-fun.dk>
2021-11-17 12:34:16 +0100

    new unit test

--- tests/send_data_test.py
+++ tests/send_data_test.py
@@ -1,10 +1,20 @@
+import os
+import sys
 import unittest
 
+from config_data import config
+import send_json
+import segment as seg
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
+class SendDataTest(unittest.TestCase):
+    def test_pingserver(self):
+        response = os.system("ping -c 1 130.225.57.27")
+        self.assertEqual(response, 0)
+        
+    def test_leigtemptySchema(self):
+        config["OUTPUT_FOLDER"] = "data/emptyJsonValid/"
+        send_json.send_data()
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
