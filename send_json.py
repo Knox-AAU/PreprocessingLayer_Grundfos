ee676595149e6c0ed29e7b96919702907fe36b5b
itDaveDK <git@david-fun.dk>
2021-11-25 19:30:37 +0100

    Updated test and changed so json test files is not ignored

--- send_json.py
+++ send_json.py
@@ -9,7 +9,7 @@ from config_data import config
 """
 Send data to Knowledge Layer
 """
-def send_data():
+def send_data(url:str = "http://127.0.0.1:8000/uploadjsonapi/uploadJsonDoc"):
     output_folder = config["OUTPUT_FOLDER"]
     for foldername in os.listdir(output_folder):
         id = foldername[len("Grundfosliterature-"):]
@@ -17,7 +17,7 @@ def send_data():
         json_path = os.path.join(output_folder, foldername, json_file_name)
         try:
             with open(json_path, "r", encoding="utf-16") as json_file:
-                IOHandler.post_json(json_file.read().encode("utf-8"), 'http://130.225.57.27/uploadjsonapi/uploadJsonDoc')
+                IOHandler.post_json(json_file.read().encode("utf-8"), url)
         except FileNotFoundError:
             pass
 
--- tests/send_data_test.py
+++ tests/send_data_test.py
@@ -20,10 +20,20 @@ class SendDataTest(unittest.TestCase):
     def test_legitEmptySchema(self):
         config["OUTPUT_FOLDER"] = os.path.join(os.getcwd(), "tests", "data", "emptyJsonValid")
         try:
-            send_json.send_data()
-        except requests.exceptions.HTTPError as e:
-            self.fail(("Knowledge layer server returned unexpected HTTP response code.\n" +
-                       "Test data send can be found in folder: " + config["OUTPUT_FOLDER"]))
+            send_json.send_data("http://127.0.0.1:8000/uploadJsonDoc/")
+        except requests.exceptions.HTTPError:
+            self.fail(self.failDataToKnowledgeLayerMsg())
+            
+    def test_invalidEmptySchema(self):
+        config["OUTPUT_FOLDER"] = os.path.join(os.getcwd(), "tests", "data", "jsonInvalid")
+        with self.assertRaises(requests.exceptions.HTTPError):
+            send_json.send_data("http://127.0.0.1:8000/uploadJsonDoc/")
+    
+    def failDataToKnowledgeLayerMsg():
+        return ("Knowledge layer server returned unexpected HTTP response code.\n" +
+                "Test data send can be found in folder: " + config["OUTPUT_FOLDER"])
+    
+
 
 if __name__ == '__main__':
     unittest.main()
