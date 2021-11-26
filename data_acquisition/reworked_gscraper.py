2fec2cc2fe894c42386e881189f035c9d06fe70b
Nick / AFK <ndja19@student.aau.dk>
2021-11-26 20:58:51 +0100

    new Grundfos scraper

--- data_acquisition/reworked_gscraper.py
+++ data_acquisition/reworked_gscraper.py
@@ -1,34 +1,76 @@
+import os
+import random
+from time import sleep
+
 import requests
 
 FILETYPE = '.pdf'
 DOMAIN = 'https://www.grundfos.com'
-MAX_SITES_TO_CHECK = 7000001
+MAX_SITES_TO_CHECK = 100 #7000001 #7 million is chosen just to get a good sample size, however this process make time LONG time
+ITERATIONS_BEFORE_DELAY = 20 #Choose whatever works so the API doensn't block the connections because of insane amount of pings
+MIN_SLEEP = 10
+MAX_SLEEP = 20
 PDF_DOMAIN = "http://net.grundfos.com/Appl/ccmsservices/public/literature/filedata/Grundfosliterature-"
+PATH_TO_INVALID_LINKS = os.path.join(os.path.abspath(os.curdir), "scraperdata") + "/invalid_links.txt"
+PATH_TO_VALID_LINKS = os.path.join(os.path.abspath(os.curdir), "scraperdata") + "/downloadable_links.txt"
+PATH_TO_INDEXES_CHECKED = os.path.join(os.path.abspath(os.curdir), "scraperdata") + "/checked_links.txt"
 
 invalid_links = set()
 valid_links = set()
 
-# http://net.grundfos.com/Appl/ccmsservices/public/literature/filedata/Grundfosliterature-425556.pdf
 
 def get_response_headers():
-    iterations = 0
-    for x in range(53):
+    index = read_index_from_file() + 1
+    for x in range(index, MAX_SITES_TO_CHECK):
         link = PDF_DOMAIN + str(x) + FILETYPE
         read = requests.get(link)
         content_type = read.headers.get("content-type")
         print(f"{x}: Content-type = {content_type}")
-        if content_type == "None":
-            invalid_links.add(x)
+        if content_type is None:
+            invalid_links.add(link + "\n")
         elif content_type == "application/pdf":
-            valid_links.add(x)
-        iterations = iterations + 1
-        if iterations >= 10:
-            print("10 iterations")
-            print(iterations % 10)
-            iterations = 0
+            valid_links.add(link + "\n")
+        if x % ITERATIONS_BEFORE_DELAY == 1:
+            delay()
+        if x == MAX_SITES_TO_CHECK - 1:
+            safe_index(x)
+
     return
+
+
+def delay():
+    sleeptimer = random.uniform(MIN_SLEEP, MAX_SLEEP)
+    print(f"Sleeping {sleeptimer} seconds")
+    sleep(sleeptimer)
+
+
+def safe_valid_links():
+    file = open(PATH_TO_VALID_LINKS, "a")
+    file.writelines(valid_links)
+    file.close()
+
+
+def safe_invalid_links():
+    file = open(PATH_TO_INVALID_LINKS, "a")
+    file.writelines(invalid_links)
+    file.close()
+
+
+def safe_index(index: int):
+    file = open(PATH_TO_INDEXES_CHECKED, "w")
+    file.write(str(index))
+    file.close()
+
+
+def read_index_from_file():
+    file = open(PATH_TO_INDEXES_CHECKED, "r")
+    index = int(file.readline())
+    file.close()
+    return index
 
 
 if __name__ == "__main__":
     print("initiating the scraper")
     get_response_headers()
+    safe_valid_links()
+    safe_invalid_links()
