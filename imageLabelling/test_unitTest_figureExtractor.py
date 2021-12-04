cf5f3b572d84536e08f515488838f029b7f43960
Zerplin <43621940+Zerplin@users.noreply.github.com>
2021-12-04 16:03:28 +0100

    added test to verify correct filename is extracted

---
+++ imageLabelling/test_unitTest_figureExtractor.py
@@ -0,0 +1,12 @@
+import unittest
+from figureExtractor import * 
+
+def testFileName():
+    #arrange
+    figureExtractor = FigureExtractor('IO/INPUT_ALL', 'IO/OUTPUT_FIG', 'IO/OUTPUT_DATA', 'OUTPUT_PRUNED')
+    testCases = [("grundfos.json", "grundfos"), ("hel-lo.world", "hel-lo"), ("_hello.world", "_hello")]
+    
+    #act
+    for case in testCases:
+        assert figureExtractor.get_filename(case[0]) == case[1]
+
