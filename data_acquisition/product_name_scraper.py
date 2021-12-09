265ead6c31922ece0bf07e33fa0282cd52bd0c36
Nick / AFK <ndja19@student.aau.dk>
2021-12-09 16:03:58 +0100

    update

--- data_acquisition/product_name_scraper.py
+++ data_acquisition/product_name_scraper.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 import requests as r
@@ -6,7 +7,7 @@ from tqdm import tqdm
 
 BASE = "https://www.manualslib.com"
 DOMAIN = "https://www.manualslib.com/brand/grundfos/"
-PATH_TO_NAMES = os.path.abspath(os.curdir) + "/products.txt"
+PATH_TO_NAMES = os.path.abspath(os.curdir) + "/products.csv"
 
 def get_href_for_categories(domain):
     links = set()
@@ -33,9 +34,7 @@ def get_product_names(containerHref):
         for cn in containers:
             try:
                 text = cn.find('a').text
-                if str(text).isdecimal():
-                    text = f"{splitDomain} {text}"
-                names.add(f"{text}\n")
+                names.add((text, splitDomain))
             except:
                 continue
         pbar.update(1)
@@ -43,13 +42,20 @@ def get_product_names(containerHref):
     return names
 
 def save_names_to_list(names, path):
+    with open(path, 'w') as out:
+        csv_out = csv.writer(out)
+        csv_out.writerow(["Name", "Tag"])
+        for row in names:
+            print(row)
+            csv_out.writerow(row)
+"""
     pbar = tqdm(total=1)
     pbar.set_description("Saving product names")
     file = open(path, "w")
     file.writelines(names)
     file.close()
     pbar.update(1)
-    pbar.close()
+    pbar.close()"""
 
 
 if __name__ == "__main__":
