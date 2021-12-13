4f26902135208e18b995bc730e0fd02ec9710b67
Zerplin <43621940+Zerplin@users.noreply.github.com>
2021-12-13 09:35:18 +0100

    Update labelGenerator.py

--- imageLabelling/labelGenerator.py
+++ imageLabelling/labelGenerator.py
@@ -3,17 +3,18 @@ import json
 import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
+import spacy
+nlp = spacy.load('en_core_web_sm')
 
 class labelGenerator:
 	def __init__(self, figureDataPath = "../OUTPUT_PRUNED"):
 		self.figureDataPath = figureDataPath
 		self.captionList = []
 		self.idList = []
-		self.load_json(self.figureDataPath)
+		self.load_json(self.figureDataPath) #TODO refactor to return tuple containing captionList and idList 
 		self.df = self.generate_scores(self.captionList, self.idList)
 		self.colList = self.df.columns.values.tolist()
-		print(self.colList)
-		print()
+		#print(self.colList)
 
 	def get_filename(self, file):
 		filename, file_extension = os.path.splitext(file)
@@ -30,6 +31,16 @@ class labelGenerator:
 				for fig in pdfJSON:
 					self.captionList.append(fig["caption"])
 					self.idList.append(fig["figID"])
+     
+	def lemmatize(self, labelList):
+		lemmatizedList = []
+		words = " ".join(labelList)
+		doc = nlp(words)
+		for token in doc:
+			if token.lemma_ not in lemmatizedList:
+				lemmatizedList.append(token.lemma_)
+    
+		return lemmatizedList
 
 	def generate_scores(self, captionList, idList):
 		myStopWords = text.ENGLISH_STOP_WORDS
@@ -47,7 +58,7 @@ class labelGenerator:
 		self.colList.reverse()
 		result = list(filter(lambda col: (self.df.loc[id, col] != 0), self.colList))
 
-		print(id)
-		print(result[0:max_labels])
-		return result[0:max_labels]
+		return self.lemmatize(result[0:max_labels])
 
+lg = labelGenerator()
+print(lg.get_label(2405, 5))
